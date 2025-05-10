6e763c498b26433cb07c7070e8f602052ca1a778
Jomar <jomarhan.ng@gmail.com>
2025-05-10 12:51:33 -0400

    install zod and build recipeSchema

---
+++ src/recipeSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const RecipeSchema = z.object({
+  name: z.string().describe("Name of the recipe"),
+  ingredients: z
+    .array(
+      z.object({
+        quantity: z.string().describe("Quantity of the ingredient"),
+        ingredient: z.string().describe("ingredient name"),
+      })
+    )
+    .describe("List of ingredients"),
+  instructions: z
+    .array(z.string())
+    .describe("Markdown content if descipbe the recipe steps")
+    .describe("steps of the recipe"),
+});
